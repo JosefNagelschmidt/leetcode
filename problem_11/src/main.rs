6e052095dc94ae9d0484a4ed22653ffe0bbc15ff
josefnagelschmidt <achill95@web.de>
2022-05-07 02:58:55 +0200

    problem_11 solved

---
+++ problem_11/src/main.rs
@@ -0,0 +1,58 @@
+use std::cmp;
+
+fn reduce(height: &Vec<i32>, remaining_indeces: &mut Vec<usize>) -> bool {
+    let n_ante = remaining_indeces.len();
+    if n_ante < 3 {
+        true;
+    }
+    let mut keep = vec![true; n_ante];
+
+    let triple_iter = remaining_indeces.windows(3);
+    triple_iter.enumerate().for_each(|(i, triple)| {
+        if (height[triple[0]] >= height[triple[1]]) & (height[triple[2]] >= height[triple[1]]) {
+            keep[i + 1] = false;
+        }
+    });
+    let mut iter = keep.iter();
+    remaining_indeces.retain(|_| *iter.next().unwrap());
+
+    if n_ante == remaining_indeces.len() {
+        true
+    } else {
+        false
+    }
+}
+
+fn get_max_area(height: &Vec<i32>, remaining_indeces: &Vec<usize>) -> i32 {
+    let remaining_heights = remaining_indeces
+        .iter()
+        .map(|i| height[*i])
+        .collect::<Vec<_>>();
+    let mut maximum = -1;
+    let mut val: i32;
+    for left in 0..remaining_indeces.len() - 1 {
+        for right in left + 1..remaining_indeces.len() {
+            val = (remaining_indeces[right] as i32 - remaining_indeces[left] as i32)
+                * (cmp::min(remaining_heights[left], remaining_heights[right]));
+            if val > maximum {
+                maximum = val;
+            }
+        }
+    }
+    return maximum;
+}
+
+fn main() {
+    pub fn max_area(height: Vec<i32>) -> i32 {
+        let mut reduced: bool = false;
+        let mut remaining_indeces: Vec<usize> = Vec::from_iter(0..height.len());
+        while !reduced {
+            reduced = reduce(&height, &mut remaining_indeces);
+        }
+        let max_area = get_max_area(&height, &remaining_indeces);
+        return max_area;
+    }
+
+    // let height = vec![1, 8, 6, 2, 5, 4, 8, 3, 7];
+    // println!("{:?}", max_area(height));
+}
